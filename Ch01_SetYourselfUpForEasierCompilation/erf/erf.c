c2b6ed14e56e6d1c870248f8bf94e15986e90836
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-03-14 19:38:22 -0700

    Format erf with clang-format

--- Ch01_SetYourselfUpForEasierCompilation/erf/erf.c
+++ Ch01_SetYourselfUpForEasierCompilation/erf/erf.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main() {
-  printf("The integral of a Normal(0, 1) distribution "
-         "between -1.96 and 1.96 is: %g\n",
-         erf(1.96 * sqrt(1 / 2.)));
+    printf("The integral of a Normal(0, 1) distribution "
+           "between -1.96 and 1.96 is: %g\n",
+           erf(1.96 * sqrt(1 / 2.)));
 }
