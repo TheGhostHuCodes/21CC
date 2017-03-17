a9d79a0374bc6ae11b897f7a12569117850976ec
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-03-16 18:09:11 -0700

    Format gsl_erf with clang-format

--- Ch01_SetYourselfUpForEasierCompilation/gsl_erf/gsl_erf.c
+++ Ch01_SetYourselfUpForEasierCompilation/gsl_erf/gsl_erf.c
@@ -2,6 +2,6 @@
 #include <stdio.h>
 
 int main() {
-  double bottom_tail = gsl_cdf_gaussian_P(-1.96, 1);
-  printf("Area between [-1.96, 1.96]: %g\n", 1 - 2 * bottom_tail);
+    double bottom_tail = gsl_cdf_gaussian_P(-1.96, 1);
+    printf("Area between [-1.96, 1.96]: %g\n", 1 - 2 * bottom_tail);
 }
