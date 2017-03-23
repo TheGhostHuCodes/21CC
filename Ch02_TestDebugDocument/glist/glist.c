df724874bd4cc41a2fe8b426864019946cdc3039
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-03-22 22:41:30 -0700

    Format glist with clang-format

--- Ch02_TestDebugDocument/glist/glist.c
+++ Ch02_TestDebugDocument/glist/glist.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
 #include <glib.h>
+#include <stdio.h>
 
 GList* list;
 
 int main() {
-  list = g_list_append(list, "a");
-  list = g_list_append(list, "b");
-  list = g_list_append(list, "c");
+    list = g_list_append(list, "a");
+    list = g_list_append(list, "b");
+    list = g_list_append(list, "c");
 
-  for (; list != NULL; list = list->next) {
-    printf("%s\n", (char*)list->data);
-  }
+    for (; list != NULL; list = list->next) {
+        printf("%s\n", (char*)list->data);
+    }
 }
