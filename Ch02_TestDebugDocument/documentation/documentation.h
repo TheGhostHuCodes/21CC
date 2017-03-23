270eaeb27479dfceb1a4ac91f484e144cea8ea18
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-03-23 07:14:59 -0700

    Add documentation and doxygen config file

---
+++ Ch02_TestDebugDocument/documentation/documentation.h
@@ -0,0 +1,23 @@
+/** \mainpage This is the main page of my doxygen documentation.
+
+\ref tag1
+
+\ref tag2
+
+\ref tag3
+
+\ref tag4
+
+*/
+
+/** \page tag1 This is a page tagged with tag1
+*/
+
+/** \page tag2 This is a page tagged with tag2
+*/
+
+/** \page tag3 This is a page tagged with tag3
+*/
+
+/** \page tag4 This is a page tagged with tag4
+*/
