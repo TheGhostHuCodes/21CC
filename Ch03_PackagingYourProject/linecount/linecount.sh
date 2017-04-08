00ebbc96afa16030eac10500f12a1dc4aad6c358
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-04-08 07:04:32 -0700

    Fix shellcheck errors in linecount script

--- Ch03_PackagingYourProject/linecount/linecount.sh
+++ Ch03_PackagingYourProject/linecount/linecount.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
+
 # Count lines with a ;, ), or }, and let that count be named Lines.
-Lines=`grep '[;)}]' *.c | wc -l`
+Lines=$(grep '[;)}]' ./*.c | wc -l)
 
 # Now count how many lines there are in a directory listing; name it Files.
-Files=`ls *.c | wc -l`
+Files=$(find . -name '*.c' | wc -l)
 
-echo files =$Files and lines =$Lines
+echo "files =$Files and lines =$Lines"
 
 # Arithmetic expansion is a double-paren.
 # In bash, the remainder is truncated; more on this later.
-echo lines/file = $(($Lines/$Files))
+echo lines/file = $((Lines/Files))
 
 # Or, use these variables in a here script.
 # By setting scale=3, answers are printed to 3 decimal places.
