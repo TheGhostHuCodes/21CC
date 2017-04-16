3682b472adab602780c786295ab07eac607960dc
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-04-16 10:44:36 -0700

    Add example of do-while loop in C

---
+++ tutorial/do_while/do_while.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void loops(int max) {
+    int i = 0;
+    do {
+        printf("Hello #%i\n", i);
+        i++;
+    } while (i < max);
+}
+
+int main() {
+    loops(3);
+    loops(0);
+}
