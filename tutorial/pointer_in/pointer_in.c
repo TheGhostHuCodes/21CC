74745da993a5e5e1fc4718f8ce26f128f1a7d378
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-04-16 11:37:23 -0700

    Add example of passing pointer to object into a function

---
+++ tutorial/pointer_in/pointer_in.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void double_in(int* in) { *in *= 2; }
+
+int main() {
+    int x[1];
+    *x = 10;
+    double_in(x);
+    printf("x now points to %i.\n", *x);
+}
